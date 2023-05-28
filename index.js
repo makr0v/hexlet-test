caca0b1060191ce3aac2f831754ae69c0259e152
makr0v <113090802+makr0v@users.noreply.github.com>
2023-05-28 17:02:17 +0300

    Step2

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+import _ from 'lodash';
+
 export default function solution(content){
   // BEGIN
  const data = content.split('\r\n').map((val) => val.split(',')).slice(1, -1)
  console.log('Count: ' + data.length);
+ 
+ const cities = _.uniq(data.map((val) => val[7]).sort());
+ console.log('Cities:' + cities.map((val) => ' ' + val));
   // END
 }
