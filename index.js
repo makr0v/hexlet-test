acb700e31b0c4869209457805f152b34d4782d2b
makr0v <113090802+makr0v@users.noreply.github.com>
2023-05-28 16:09:26 +0300

    Step1

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 export default function solution(content){
   // BEGIN
-
+ const data = content.split('\r\n').map((val) => val.split(',')).slice(1, -1)
+ console.log('Count: ' + data.length);
   // END
 }
